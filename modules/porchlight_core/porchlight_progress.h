c55e42c164a2491a2f08c5337bdee0a5dc2aa114
Blake Schmitt <85618096+blake2731@users.noreply.github.com>
2026-08-04 23:25:56 -0400

    Add batch Porchlight progress mutations

--- modules/porchlight_core/porchlight_progress.h
+++ modules/porchlight_core/porchlight_progress.h
@@ -6,12 +6,16 @@
 #include "core/templates/hash_set.h"
 #include "core/templates/vector.h"
 #include "core/variant/array.h"
+#include "core/variant/variant.h"
 
 class PorchlightProgress : public Object {
     GDCLASS(PorchlightProgress, Object);
 
     HashSet<StringName> completed_lookup;
     Vector<StringName> completed_milestones;
+
+    PackedStringArray _normalize_milestones(
+            const PackedStringArray &p_milestones) const;
 
 protected:
     static void _bind_methods();
@@ -22,8 +26,14 @@ public:
     bool complete_milestone(
             const StringName &p_milestone);
 
+    PackedStringArray complete_milestones(
+            const PackedStringArray &p_milestones);
+
     bool remove_milestone(
             const StringName &p_milestone);
+
+    PackedStringArray remove_milestones(
+            const PackedStringArray &p_milestones);
 
     bool is_complete(
             const StringName &p_milestone) const;
